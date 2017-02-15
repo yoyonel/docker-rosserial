08f5a3db453a699b2e2fa981b67741d5f8b4a725
ATTY Lionel <lionel.atty@ign.fr>
2017-02-15 16:30:26 +0100

    Clean des scripts (car utilisation de scripts template globaux).

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build \
-	--build-arg http_proxy=http://proxy.ign.fr:3128 \
-	--build-arg HTTP_PROXY=http://proxy.ign.fr:3128 \
-	--tag ros/rosserial:indigo .
--- build_and_push_in_registry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build \
-	--build-arg http_proxy=http://proxy.ign.fr:3128 \
-	--build-arg HTTP_PROXY=http://proxy.ign.fr:3128 \
-	--tag 172.20.250.99:5000/li3ds/rosserial:indigo \
-    .
-
-docker push 172.20.250.99:5000/li3ds/rosserial:indigo
