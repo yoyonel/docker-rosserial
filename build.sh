f1bdbb84cc601e718d57b76c83fce123ba40a713
ATTY Lionel <yoyonel@hotmail.com>
2017-02-20 02:58:45 +0400

    Ajout d'un fichier settings supplémentaire, qui permet de setter les var. env pour le projet de build d'une image Docker (en l'occurrence rosserial dans ce cas).

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
-DOCKER_BUILD_FROM=ros:nvidia
-DOCKER_BUILD_OPTIONS="--rm --no-cache"
-DOCKER_BUILD_TAG=rosserial
+#!/bin/bash
 
-echo "FROM $DOCKER_BUILD_FROM\n$(cat Dockerfile)" > Dockerfile.tmp
+# Load var env for image building
+source dockerbuild.env
 
+# at directive FROM at the beginning of the temp Dockerfile
+echo -e "FROM $DOCKER_BUILD_FROM\n$(cat Dockerfile)" > Dockerfile.tmp
+
+# build Docker image
 docker build    \
     $DOCKER_BUILD_OPTIONS   \
     --tag $DOCKER_BUILD_TAG \
     --file Dockerfile.tmp   \
     .
 
+# delete temp Dockerfile
 rm Dockerfile.tmp
